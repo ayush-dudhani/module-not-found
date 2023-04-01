615083b6f6900b3dc62b9492da4ec10a967c8437
Abhijit Kailas Gadhave <abhijit@Abhijits-MacBook-Air.local>
2023-04-01 15:47:39 +0530

    About Section Implementation done

--- client/src/components/Layout/Layout.jsx
+++ client/src/components/Layout/Layout.jsx
@@ -2,12 +2,15 @@ import React, { Fragment } from "react"
 import Header from "../Header/Header"
 import Routers from "../../routers/Routers"
 import Footer from "../Footer/Footer"
+import About from "../../pages/About"
 const Layout = () => {
   return (
     <Fragment>
+      
       <Header />
 
       <Routers />
+      <About/>
 
       <Footer />
     </Fragment>
