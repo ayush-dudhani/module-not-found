ee653e0a64df245ec126816c6b004c2e99290774
Vipul <gaikwadvipul196@gmail.com>
2023-04-01 15:52:03 +0530

    changes added

--- client/src/components/UI/HeroSlilder.jsx
+++ client/src/components/UI/HeroSlilder.jsx
@@ -1,5 +1,4 @@
 import React from "react"
-
 import Slider from "react-slick"
 import { Container } from "reactstrap"
 import { Link } from "react-router-dom"
--- client/src/pages/Home.jsx
+++ client/src/pages/Home.jsx
@@ -1,7 +1,13 @@
 import React from "react"
 import Helmet from "../components/Helmet/Helmet"
+import HeroSlider from "../components/UI/HeroSlilder"
 const Home = () => {
-  return <Helmet title="Home">{/* <HeroSlider /> */}</Helmet>
+  return (
+    <Helmet title="Home">
+      {" "}
+      <HeroSlider />{" "}
+    </Helmet>
+  )
 }
 
 export default Home
