d410a2f46c03817cba4072794525a621983139cb
Vipul <gaikwadvipul196@gmail.com>
2023-04-01 17:02:41 +0530

    home page about section fixed

--- client/src/pages/Home.jsx
+++ client/src/pages/Home.jsx
@@ -1,11 +1,12 @@
 import React from "react"
 import Helmet from "../components/Helmet/Helmet"
 import HeroSlider from "../components/UI/HeroSlilder"
+import AboutSection from "../components/UI/AboutSection"
 const Home = () => {
   return (
     <Helmet title="Home">
       {" "}
-      <HeroSlider />{" "}
+      <HeroSlider /> <AboutSection />
     </Helmet>
   )
 }
